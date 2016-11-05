104bd1c1c4b3feb75ecf7d064cb07a4e26dcaa2e
Ryan Hammarskjold <Ryan Hammarskjold>
2016-11-05 16:38:05 -0400

    filing is better, now has testing file

--- SecretSanta.py
+++ SecretSanta.py
@@ -1,32 +1,12 @@
-import random
 import sys
 import os
 
-#randomizes such that names[i] != to[i] for any i
-def randomize(names, to):
-	
-	random.shuffle(to)
-	random.shuffle(names)
+from RandomSanta import randomize
 
-	for index in range(0, len(names) - 1):
-		if names[index] == to[index]:
-			temp = to[index] 
-			to[index] = to[index + 1]
-			to[index + 1] = temp
-
-	index = len(names) - 1
-	if names[index] == to[index]:
-		temp = to[index] 
-		to[index] = to[0]
-		to[0] = temp
-
-
-def main():
-	file = open(sys.argv[1], "r")
+def main(namesFile, dirName):
+	file = open(namesFile, "r")
 	names = file.read().splitlines()
 
-
-	dirName = "%s" % sys.argv[2]
 	os.mkdir(dirName)
 
 	to = list(names)
@@ -39,4 +19,4 @@ def main():
 		file.write(contents)
 		file.close()
 
-main()
+main(sys.argv[1], sys.argv[2])
--- testing.py
+++ testing.py
@@ -1,1 +1,46 @@
-from SecretSanta import randomize
+from RandomSanta import randomize
+import numpy as nd
+
+n = 10000
+elements = 10
+
+def checkCorrect():
+	names = list(range(elements))
+	to = list(names)
+
+	for i in range(n):
+		randomize(names, to)
+
+		for j in range(elements):
+			if names[j] == to[j]:
+				return False
+
+	return True
+
+def checkRandomFromOrder():
+
+	records = nd.zeros((elements, elements))
+	names = list(range(elements))
+	to = list(names)
+
+	for i in range(n):
+		randomize(names, to)
+
+		for j in range(elements):
+			records[names[j]][to[j]] += 1
+
+	for row in records:
+		for record in row:
+			record = record / elements
+
+	return records
+
+correct = checkCorrect()
+if not correct:
+	print "incorrect"
+
+randOrderRecords = checkRandomFromOrder()
+print randOrderRecords
+
+
+
