ad43feb07d16b54a92767a024366d5314759ba29
Ryan Hammarskjold <Ryan Hammarskjold>
2016-11-05 15:10:59 -0400

    better functioning layout

--- SecretSanta.py
+++ SecretSanta.py
@@ -2,32 +2,41 @@ import random
 import sys
 import os
 
-file = open(sys.argv[1], "r")
-names = file.read().splitlines()
+#randomizes such that names[i] != to[i] for any i
+def randomize(names, to):
+	
+	random.shuffle(to)
+	random.shuffle(names)
 
-to = list(names)
+	for index in range(0, len(names) - 1):
+		if names[index] == to[index]:
+			temp = to[index] 
+			to[index] = to[index + 1]
+			to[index + 1] = temp
 
-random.shuffle(to)
-random.shuffle(names)
-
-dirName = "%s" % sys.argv[2]
-os.mkdir(dirName)
-
-for index in range(0, len(names) - 1):
+	index = len(names) - 1
 	if names[index] == to[index]:
 		temp = to[index] 
-		to[index] = to[index + 1]
-		to[index + 1] = temp
+		to[index] = to[0]
+		to[0] = temp
 
-index = len(names) - 1
-if names[index] == to[index]:
-	temp = to[index] 
-	to[index] = to[0]
-	to[0] = temp
 
-for index in range(0, len(names) - 1):
-	fname = "%s/%s.txt" % (dirName, names[index])
-	contents = "%s, please get a gift for %s" % (names[index], to[index])
-	file = open(fname, "w")
-	file.write(contents)
-	file.close()
+def main():
+	file = open(sys.argv[1], "r")
+	names = file.read().splitlines()
+
+
+	dirName = "%s" % sys.argv[2]
+	os.mkdir(dirName)
+
+	to = list(names)
+	randomize(names, to)
+
+	for index in range(0, len(names) - 1):
+		fname = "%s/%s.txt" % (dirName, names[index])
+		contents = "%s, please get a gift for %s" % (names[index], to[index])
+		file = open(fname, "w")
+		file.write(contents)
+		file.close()
+
+main()
